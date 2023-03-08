03c4aab12d6c6949fb9f1e66b9943e644eabd430
Thiago Ribeiro <thiagoribeiromw@gmail.com>
2023-03-08 10:33:23 -0300

    Add files via upload

---
+++ Day 2 - Goals what we will make by the end of the day/day-2-end.py
@@ -0,0 +1,40 @@
+# Tipos de Dados
+
+# # num_char = len(input("What is your name?"))
+# # new_num_char = str(num_char)
+# # print("Your name has " + new_num_char + " characters")
+# # # print(type(num_char))
+
+# a = float(123)
+# print(type(a))
+
+# print(70 + float("100.5"))
+# print(str(70) + str(100))
+
+# Operações Aritméticas
+
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 2
+
+# PEMDAS
+# Parênteses ()
+# Expoentes **
+# Multiplicação *
+# Divisão /
+# Adição +
+# Subtração -
+
+# print(3 * 3 + 3 / 3 - 3)
+# print(3 * (3 + 3) / 3 - 3)
+
+# Manipulação de números e F Strings
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(
+    f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
