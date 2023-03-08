1285f280382988468d8ef83c61283d4961eb1db7
Thiago Ribeiro <thiagoribeiromw@gmail.com>
2023-03-08 10:29:32 -0300

    Create main.py

---
+++ Day 1 - Working with Variables in Python to Manage Data/main.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator.')
+
+city = input('What is the name of the city you grew up?\n')
+
+pet = input('What is the name of one of your pets?\n')
+
+print('The name of your band is: ', city + ' ' + pet)
